55273243bc332be3acc2a2f494977851dc4d0f3e
Joshua <joshua.richardson@myunidays.com>
2021-09-04 16:14:40 +0100

    Added exception callback message

--- src/graphs/time.js
+++ src/graphs/time.js
@@ -16,42 +16,47 @@ async function getTimeGraph(callback, days) {
         }))
 
         response.on('end', async () => {
-            var parsedData = JSON.parse(data);
+            try {
+                var parsedData = JSON.parse(data);
 
-            console.log(parsedData)
-
-            const barChart = new QuickChart();
-            barChart.setConfig(parsedData.barGraph);
-            barChart.setBackgroundColor("rgb(0,0,0)")
-            const barChartUrl = await barChart.getShortUrl();
-        
-            console.log(barChartUrl)
-
-            const pieChart = new QuickChart();
-            pieChart.setConfig(parsedData.pieChart)
-            pieChart.setBackgroundColor("rgb(0,0,0)")
-            const pieChartUrl = await pieChart.getShortUrl();
-
-            const barChartMessage = new Discord.MessageEmbed()
-                .setTitle("Time graph")
-                .setColor("#608b81")
-                .setImage(barChartUrl)
-                .setDescription(`Here are the results for the previous ${days} days`);
-
-            const pieChartMessage = new Discord.MessageEmbed()
-                .setTitle("Most active for time period")
-                .setColor("#608b81")
-                .setImage(pieChartUrl)
-                .setDescription(`Collated activity for the previous ${days} days`);
-
-            const championMessage = new Discord.MessageEmbed()
-                .setTitle("We have a champion :crown:")
-                .setColour(parsedData.champion.color)
-                .setDescription(`The most active user for the previous ${days} days was ${parsedData.champion.name} with an active time of ${parsedData.champion.timeActive} what a champion :crown:`);
-        
-            callback(barChartMessage);
-            callback(pieChartMessage);
-            callback(championMessage);
+                console.log(parsedData)
+    
+                const barChart = new QuickChart();
+                barChart.setConfig(parsedData.barGraph);
+                barChart.setBackgroundColor("rgb(0,0,0)")
+                const barChartUrl = await barChart.getShortUrl();
+            
+                console.log(barChartUrl)
+    
+                const pieChart = new QuickChart();
+                pieChart.setConfig(parsedData.pieChart)
+                pieChart.setBackgroundColor("rgb(0,0,0)")
+                const pieChartUrl = await pieChart.getShortUrl();
+    
+                const barChartMessage = new Discord.MessageEmbed()
+                    .setTitle("Time graph")
+                    .setColor("#608b81")
+                    .setImage(barChartUrl)
+                    .setDescription(`Here are the results for the previous ${days} days`);
+    
+                const pieChartMessage = new Discord.MessageEmbed()
+                    .setTitle("Most active for time period")
+                    .setColor("#608b81")
+                    .setImage(pieChartUrl)
+                    .setDescription(`Collated activity for the previous ${days} days`);
+    
+                const championMessage = new Discord.MessageEmbed()
+                    .setTitle("We have a champion :crown:")
+                    .setColour(parsedData.champion.color)
+                    .setDescription(`The most active user for the previous ${days} days was ${parsedData.champion.name} with an active time of ${parsedData.champion.timeActive} what a champion :crown:`);
+            
+                callback(barChartMessage);
+                callback(pieChartMessage);
+                callback(championMessage);
+            } catch (e) {
+                callback(e);
+            }
+            
 
         })
     })
