b68d112f3118499c94d37837150aa6e7d71933a2
Joshua <joshuaquarryhouse@gmail.com>
2021-09-10 12:22:05 +0100

    Added ability to DM Little Baby Botua

--- index.js
+++ index.js
@@ -230,100 +230,124 @@ bot.on('voiceStateUpdate', (oldMember, newMember) => {
 
 
 bot.on('message', function(message) {
-    let lowerCaseMessage = message.content.toLowerCase()
-
+    
     if(message.author.id === bot.user.id) {
         return;
     }
+    
+    console.log(message.guild);
+    
+    if (message.guild === null) {
+        sendLocalMessage(message)
+        return;
+    }
+    
+    sendGuildMessage();
 
-    if(lowerCaseMessage === '/help') {
-        let supportedCommands = [
-            new Commands('/cat', 'Get a random cat picture'),
-            new Commands('/wilbur', 'Get a random picture of Wilbur'),
-            new Commands('/dilbert', 'Get the daily Dilbert photo'),
-            new Commands('/echo', 'Something silly and meaningless'),
-            new Commands('/weather <City>', 'Get the weather for a specified city'),
-            new Commands('/weather <latitude> <longitude>', 'Get the weather at a specific latitude and longitude')
-        ]
-                
-        if(message.guild.id === "329759300526407680") {     
-            supportedCommands.push(new Commands('/joshua', 'Get the little baby Joshua video'));
-            supportedCommands.push(new Commands('/update', `Get an update about Joshua's current work in progress from Trello`));
+    function sendCustomDirectMessage(message, userId) {
+        bot.users.cache.get(userId).send(message);
+    }
+
+    function sendLocalMessage(message) {
+        let lowerCaseMessage = message.content.toLowerCase()
+        if (lowerCaseMessage === "/time") {
+            timeGraph.getTimeGraph((messageContent) => {
+                sendCustomDirectMessage(messageContent, message.author.id)
+            }, 7);
+        }
+    }
+
+    function sendGuildMessage() {
+        let lowerCaseMessage = message.content.toLowerCase()
+        if(lowerCaseMessage === '/help') {
+            let supportedCommands = [
+                new Commands('/cat', 'Get a random cat picture'),
+                new Commands('/wilbur', 'Get a random picture of Wilbur'),
+                new Commands('/dilbert', 'Get the daily Dilbert photo'),
+                new Commands('/echo', 'Something silly and meaningless'),
+                new Commands('/weather <City>', 'Get the weather for a specified city'),
+                new Commands('/weather <latitude> <longitude>', 'Get the weather at a specific latitude and longitude')
+            ]
+
+            if(message.guild.id === "329759300526407680") {     
+                supportedCommands.push(new Commands('/joshua', 'Get the little baby Joshua video'));
+                supportedCommands.push(new Commands('/update', `Get an update about Joshua's current work in progress from Trello`));
+            }
+            
+            let helpMessage = "Here is a list of commands:\n";
+            supportedCommands.forEach(function(command) {
+                helpMessage += `${command.command} : ${command.description}\n`;
+            });
+
+            sendMessage(helpMessage)        
+        }
+
+        if(lowerCaseMessage === '/wilbur') {
+            wilbur.createWilburCard(sendMessage);
+        }
+
+        if (lowerCaseMessage === "#time") {
+            timeGraph.getTimeGraph(sendMessage, 7);
         }
         
-        let helpMessage = "Here is a list of commands:\n";
-        supportedCommands.forEach(function(command) {
-            helpMessage += `${command.command} : ${command.description}\n`;
-        });
+        if(lowerCaseMessage === '/dilbert') {
+            dilbert.getDilbertStrip(sendMessage);
+        }
+        
+        if(lowerCaseMessage === '/cat') {
+            cat.getRandomCatImage(sendMessage);
+        }
 
-        sendMessage(helpMessage)        
-    }
+        if(lowerCaseMessage === '/echo') {
+            message.channel.send('ECHO');
+            message.channel.send('ECHo');
+            message.channel.send('ECho');
+            message.channel.send('Echo');
+            message.channel.send('echo');
+        }
 
-    if(lowerCaseMessage === '/wilbur') {
-        wilbur.createWilburCard(sendMessage);
-    }
+        if(lowerCaseMessage.includes('weather')) {
+            if(lowerCaseMessage === '/weather') {
+                sendMessage("Please specify more either a city or latitude and longitude for example:");
+                sendMessage("/weather Carlisle");
+                sendMessage("/weather 54.476422 -3.042244");
+            } else {
+                var splitMessage = lowerCaseMessage.split(' ');
 
-    if (lowerCaseMessage === "#time") {
-        timeGraph.getTimeGraph(sendMessage);
-    }
-    
-    if(lowerCaseMessage === '/dilbert') {
-        dilbert.getDilbertStrip(sendMessage);
-    }
-    
-    if(lowerCaseMessage === '/cat') {
-        cat.getRandomCatImage(sendMessage);
-    }
-
-    if(lowerCaseMessage === '/echo') {
-        message.channel.send('ECHO');
-        message.channel.send('ECHo');
-        message.channel.send('ECho');
-        message.channel.send('Echo');
-        message.channel.send('echo');
-    }
-
-    if(lowerCaseMessage.includes('weather')) {
-        if(lowerCaseMessage === '/weather') {
-            sendMessage("Please specify more either a city or latitude and longitude for example:");
-            sendMessage("/weather Carlisle");
-            sendMessage("/weather 54.476422 -3.042244");
-        } else {
-            var splitMessage = lowerCaseMessage.split(' ');
-
-            if (splitMessage.length === 3) {
-                let lat = splitMessage[1];
-                let lon = splitMessage[2];
-                weather.getWeatherByLatLon(sendMessage, lat, lon);
-            } else if (splitMessage.length === 2) {
-                let cityName = splitMessage[1];
-                weather.getWeatherByCity(sendMessage, cityName);
+                if (splitMessage.length === 3) {
+                    let lat = splitMessage[1];
+                    let lon = splitMessage[2];
+                    weather.getWeatherByLatLon(sendMessage, lat, lon);
+                } else if (splitMessage.length === 2) {
+                    let cityName = splitMessage[1];
+                    weather.getWeatherByCity(sendMessage, cityName);
+                }
             }
         }
-    }
-    
-    if(message.guild.id !== "329759300526407680") {
-        return;
-    }
+        
+        if(message.guild.id !== "329759300526407680") {
+            return;
+        }
 
-    allowedChannels = ['joshuas-updates', 'meme-spam', "bits-n-bots"]
+        allowedChannels = ['joshuas-updates', 'meme-spam', "bits-n-bots"]
 
-    // Don't send the following messages if in a forbidden channel
-    if (!allowedChannels.includes(message.channel.name)) {
-        return;
-    }
+        // Don't send the following messages if in a forbidden channel
+        if (!allowedChannels.includes(message.channel.name)) {
+            return;
+        }
 
-    if(lowerCaseMessage.includes("jordan")) {
-        friendApi.getJordan(sendMessage)
-    }
-    
-    // Server specific magic
-    if(lowerCaseMessage.includes('joshua')) {
-        message.channel.send('https://drive.google.com/file/d/1JQ3lYbHxGa-KbctUAtepN1R-rPA5fB9r/view?usp=sharing');
-    }
+        if(lowerCaseMessage.includes("jordan")) {
+            friendApi.getJordan(sendMessage)
+        }
+        
+        // Server specific magic
+        if(lowerCaseMessage.includes('joshua')) {
+            message.channel.send('https://drive.google.com/file/d/1JQ3lYbHxGa-KbctUAtepN1R-rPA5fB9r/view?usp=sharing');
+        }
 
-    function sendMessage(data) {
-        message.channel.send(data);
+        function sendMessage(data) {
+            message.channel.send(data);
+        }
     }
 });
 
